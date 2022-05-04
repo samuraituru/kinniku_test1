9acdbb187207e5d00a9c66584ce71b917d2763cf
“samuraituru <samuraituru.dev@gmail.com>
2022-05-04 22:29:41 +0900

    各ページ一新2

--- lib/list/list_page.dart
+++ lib/list/list_page.dart
@@ -1,20 +1,37 @@
 import 'package:flutter/material.dart';
-import 'package:kinnikunikki_test/tabpage/root_page.dart';
+import 'package:kinnikunikki_test/list/list_model.dart';
+import 'package:provider/provider.dart';
 
 class ListPge extends StatelessWidget {
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        centerTitle: true,
-        title: Text(
-            '一覧'
-        ),
-      ),
-      body: Center(
-        child: Text('3'),
+    return MaterialApp(
+      home: ChangeNotifierProvider<ListModel>(
+          create: (_) => ListModel(),
+          child: Scaffold(
+            appBar: AppBar(
+              centerTitle: true,
+              title: Text(
+                  '一覧'
+              ),
+            ),
+            body: Consumer<ListModel>(builder: (context, model, child) {
+                return Container(
+                  width: double.infinity,
+                  child: ListView.builder(
+                    itemCount: model.items.length,
+                    itemBuilder: (context, index) {
+                      return ListTile(
+                        title: Text('${model.items[index]}'),
+                      );
+                    },
+                  ),
+                );
+              }
+            ),
+          )
       ),
     );
   }
-
 }
